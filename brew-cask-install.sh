bac33af508549e3ae9c2fb41385271815034898f
shinyay <shinya.com@gmail.com>
2017-01-17 09:42:16 +0900

    Modify change list to array

--- brew-cask-install.sh
+++ brew-cask-install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-brew cask install atom
-brew cask install iterm2
-brew cask install boostnote
+## Applications
+apps=(
+  atom
+  iterm2
+  boostnote
+)
+
+brew cask install ${apps[@]}
+brew cask cleanup
+brew cask list
