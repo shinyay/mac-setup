5c44864bc1e27955c2b636c19edd184da927b8cc
shinyay <shinya.com@gmail.com>
2017-01-17 10:42:50 +0900

    Add Alfred and Chrome-Canary

--- brew-cask-install.sh
+++ brew-cask-install.sh
@@ -5,8 +5,13 @@ apps=(
   atom
   iterm2
   boostnote
+  alfred
 )
 
 brew cask install ${apps[@]}
+
+brew tap caskroom/versions
+brew cask install google-chrome-canary
+
 brew cask cleanup
 brew cask list
